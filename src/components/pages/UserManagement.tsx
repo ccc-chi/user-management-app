2e084bf857ad92d908525dab15ec48a58d52b3c4
chihiro <chieeie@gmail.com>
2025-02-06 18:43:01 +0900

    ユーザー取得

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,38 @@
-import { FC, memo } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect, useRef } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../Hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  const connectionRef = useRef(false);
+  useEffect(() => {
+    if (!connectionRef.current) {
+      getUsers();
+      connectionRef.current = true;
+    }
+  }, []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://picsum.photos/400"
-          name="じゃっけ"
-          fullName="jake name"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h={"100vh"}>
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx={"auto"}>
+              <UserCard
+                imageUrl="https://picsum.photos/400"
+                name={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
